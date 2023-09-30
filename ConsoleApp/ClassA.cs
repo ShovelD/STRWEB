a913be3f5cccf10ce4e44e7e5b0843fa48c15024
ShovelD <gimnazist.ds@gmail.com>
2023-09-30 15:34:07 +0300

    Added some new stuff

---
+++ ConsoleApp/ClassA.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp
+{
+    internal class ClassA
+    {
+        public int CLassAIntProperty { get;private set; } = 1;
+        public string CLassAStringProperty { get; private set; } = "string";
+
+        public ClassA(int i) 
+        {
+            CLassAIntProperty = i;
+        }
+
+        public override string ToString()
+        {
+            return CLassAIntProperty.ToString() + CLassAStringProperty;
+        }
+    }
+}
