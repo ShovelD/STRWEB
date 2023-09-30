a876846ce709071b1909418cfa258ca0b04b09b4
ShovelD <gimnazist.ds@gmail.com>
2023-09-30 15:47:24 +0300

    fixxed compile errors

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,11 +7,7 @@ internal class Program
     {
         ClassA classAObject = new(5);
         Console.WriteLine(classAObject);
-<<<<<<< HEAD
-        Console.WriteLine(classAObject.CLassAIntProperty * 24;
+        Console.WriteLine(classAObject.CLassAIntProperty * 24);
         Console.WriteLine(classAObject.CLassAIntProperty * Math.PI / 2);
-=======
-       
->>>>>>> 5a913114829b16eecb5f808287414bfdc5baf0a9
     }
 }
